f289642431ddc99cf579a41fa39a0d1f93ed9de1
Adesh Mishra <adesh.mishra@quantinent.ai>
2026-06-12 00:29:38 +0530

    Add transaction list

--- src/components/TransactionList.jsx
+++ src/components/TransactionList.jsx
@@ -2,7 +2,29 @@ function TransactionList({ transactions }) {
   return (
     <div className="card">
       <h2>Transactions</h2>
-      <p>Total transactions: {transactions.length}</p>
+
+      {transactions.length === 0 ? (
+        <p>No transactions added yet.</p>
+      ) : (
+        <div className="transaction-list">
+          {transactions.map((transaction) => (
+            <div className="transaction-item" key={transaction.id}>
+              <div>
+                <strong>{transaction.category}</strong>
+                <p>{transaction.note || "No note"}</p>
+              </div>
+
+              <div>
+                <strong>
+                  {transaction.type === "income" ? "+" : "-"}₹
+                  {transaction.amount}
+                </strong>
+                <p>{transaction.date}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
